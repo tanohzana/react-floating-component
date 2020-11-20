a3c2311112ea18566b366d5120f85734a5c52c21
Florian Adonis <tanohzana@gmail.com>
2020-11-20 08:47:47 +0000

    Updates

---
+++ precache-manifest.a5e1638f76f1c63475b90b6e104c9082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e031547018ad4549f1ef0be8bdaf3e76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "423862f54f2fb3132509",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "e833e8baed98cd131531",
+    "url": "./static/js/2.db67c04e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.db67c04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423862f54f2fb3132509",
+    "url": "./static/js/main.5add9ba8.chunk.js"
+  },
+  {
+    "revision": "45976b0aa08d5e5d6c0b",
+    "url": "./static/js/runtime-main.91ec2896.js"
+  }
+]);
